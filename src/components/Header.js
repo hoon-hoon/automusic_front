5501e5efb5989d9f3f5b2a9db6b7e46ffdd883c7
hoon-hoon <kc0393@naver.com>
2023-02-25 23:46:22 +0900

    header complete

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,14 +6,16 @@ import {
   Divider,
   Box,
   Grid,
+  Link,
   Tabs,
   Tab,
 } from "@mui/material";
 import logo from "../asset/logo.png";
-import { Navigate } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import { styled } from "@mui/material/styles";
 
 function Header() {
+
   const StyledTabs = styled((props) => (
     <Tabs
       {...props}
@@ -34,93 +36,120 @@ function Header() {
     },
   });
 
-  const StyledTab = styled((props) => <Tab disableRipple {...props} />)(
-    ({ theme }) => ({
-      textTransform: "none",
-      fontWeight: theme.typography.fontWeightRegular,
-      fontSize: theme.typography.pxToRem(15),
-      marginRight: theme.spacing(1),
-      color: "black",
-      "&.Mui-selected": {
-        color: "red",
-      },
-      "&.Mui-focusVisible": {
-        backgroundColor: "rgba(100, 95, 228, 0.32)",
-      },
-    })
-  );
+  const StyledTab = styled((props) => (
+    <Tab disableRipple component={NavLink} to={props.to} {...props} />
+  ))(({ theme }) => ({
+    textTransform: "none",
+    fontWeight: theme.typography.fontWeightRegular,
+    fontSize: theme.typography.pxToRem(15),
+    marginRight: theme.spacing(1),
+    color: "black",
+    "&.Mui-selected": {
+      color: "red",
+    },
+    "&.Mui-focusVisible": {
+      backgroundColor: "rgba(100, 95, 228, 0.32)",
+    },
+  }));
+
   const [value, setValue] = useState(0);
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
   };
 
+  const currentPathname = window.location.pathname;
+
   return (
-    <>
-      <div id="header">
-        <Box
-          sx={{
-            display: "grid",
-            alignItems: "center",
-            width: "100%",
-            borderRadius: 1,
-            color: "text.secondary",
-            "& svg": {
-              m: 1.5,
-            },
-            "& hr": {
-              mx: 0.5,
-            },
-          }}
-        >
-          <Grid container spacing={10}>
-            <Grid item xs={1}>
-              <Typography
-                align="left"
-                style={{
-                  height: "40px",
-                  width: "fullWidth",
-                  position: "fixed",
-                }}
-              >
-                <a href="/">
-                  <img
-                    href="/"
-                    src={logo}
-                    style={{ marginTop: "0%", height: "40px" }}
-                  ></img>
-                </a>
-              </Typography>
-            </Grid>
-            <Grid item xs={1}></Grid>
-            <Grid item xs={7}>
-              <StyledTabs
-                value={value}
-                onChange={handleChange}
-                aria-label="full width tabs example"
-                variant="fullWidth"
-                indicatorColor="secondary"
-              >
-                <StyledTab label="HOME" href="/"></StyledTab>
-                <StyledTab label="CREATE" href="/create"></StyledTab>
-                <StyledTab label="CHART" href="/chart"></StyledTab>
-                <StyledTab label="BOARD" href="/board"></StyledTab>
-              </StyledTabs>
-            </Grid>
-
-            <Grid item xs={1}>
-              <Divider orientation="vertical" />
-            </Grid>
-
-            <Grid item xs={2}>
-              <Button align="right">
-                <a href="/login">로그인</a>
-              </Button>
-            </Grid>
+    <div id="header">
+      <Box
+        sx={{
+          display: "grid",
+          alignItems: "center",
+          width: "100%",
+          borderRadius: 1,
+          color: "text.secondary",
+          "& svg": {
+            m: 1.5,
+          },
+          "& hr": {
+            mx: 0.5,
+          },
+        }}
+      >
+        <Grid container spacing={10}>
+          <Grid item xs={1}>
+            <Typography
+              align="left"
+              style={{
+                height: "40px",
+                width: "fullWidth",
+                position: "fixed",
+              }}
+            >
+              <Link href="/">
+                <img
+                  href="/"
+                  src={logo}
+                  style={{ marginTop: "0%", height: "40px" }}
+                ></img>
+              </Link>
+            </Typography>
           </Grid>
-        </Box>
-      </div>
-    </>
+          <Grid item xs={1}></Grid>
+          <Grid item xs={7}>
+            <StyledTabs
+              value={value}
+              onChange={handleChange}
+              aria-label="full width tabs example"
+              variant="fullWidth"
+              indicatorColor="secondary"
+            >
+              <StyledTab
+                label="HOME"
+                to="/"
+                selected={currentPathname === "/"}
+              />
+              <StyledTab
+                label="CREATE"
+                to="/create"
+                selected={currentPathname === "/create"}
+              />
+              <StyledTab
+                label="CHART"
+                to="/chart"
+                selected={currentPathname === "/chart"}
+              />
+              <StyledTab
+                label="BOARD"
+                to="/board"
+                selected={currentPathname === "/board"}
+              />
+            </StyledTabs>
+          </Grid>
+          <Grid item xs={1}>
+            <Divider orientation="vertical"/>
+          </Grid>
+          <Grid item xs={2}>
+            <Link href="/login">
+            <Button
+              variant="contained"
+              style={{
+                float: "right",
+                backgroundColor: "#bf1650",
+                color: "white",
+                fontWeight: "bold",
+                borderRadius: 20,
+                marginRight: "3vw",
+              }}
+            >
+              LOGIN
+            </Button>
+            </Link>
+          </Grid>
+        </Grid>
+      </Box>
+    </div>
   );
 }
 
