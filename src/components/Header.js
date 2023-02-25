0dd0b5241ccd738bfd5c5b2c21d673339b6f50a4
hoon-hoon <kc0393@naver.com>
2023-02-25 20:18:08 +0900

    setup

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,30 @@
-import './Header.css';
+import "./Header.css";
 import React from "react";
-import { Typography } from '@mui/material';
-import logo from '../asset/logo.png';
+import { AppBar, Typography, Button, Divider } from "@mui/material";
+import logo from "../asset/logo.png";
 
 function Header() {
-    return(<>
-        <div id="header">
-        <Typography align="left" style={{width:"100%"}}>
-            <a href="/" >
-              <img href="/" src={logo} style={{marginTop:"0%", height:"40px"}}></img>
-            </a>
+  return (
+    <>
+      <div id="header">
+        <Typography align="left" position={"absolute"}>
+          <a href="/">
+            <img
+              href="/"
+              src={logo}
+              style={{ marginTop: "0%", height: "40px" }}
+            ></img>
+          </a>
         </Typography>
-
-        </div>
-    </>)
+        <Divider>
+            
+        </Divider>
+        <Typography align="right" marginRight={"3vw"}>
+          <a href="/login">로그인</a>
+        </Typography>
+      </div>
+    </>
+  );
 }
 
 export default Header;
