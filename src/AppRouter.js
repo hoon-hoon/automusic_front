025d8730f2cfd5ffe27792f92ea730f278c85f0d
yelim1215 <yerim1215jg@naver.com>
2023-04-02 16:29:08 +0900

    test git

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -1,28 +1,46 @@
 import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Outlet, Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import Board from "./pages/Board";
 import Chart from "./pages/Chart";
 import Create from "./pages/Create";
 import Login from "./pages/Login";
 import Main from "./pages/Main";
+import Signup from "./pages/Signup";
+
+function LoginLayout() {
+  return <Outlet />;
+}
+
+function BasicLayout() {
+  return (
+    <>
+      <Header />
+      <body style={{ marginTop: "80px" }}>
+        <Outlet />
+      </body>
+    </>
+  );
+}
 
 function AppRouter() {
-    return (
-        <BrowserRouter>
-            <Header />
-            <body style={{marginTop:"80px"}}>
-            <Routes>
-                <Route path="/" element={<Main />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/board" element={<Board />} />
-                <Route path="/chart" element={<Chart />} />
-                <Route path="/create" element={<Create />} />
-
-            </Routes>
-            </body>
-        </BrowserRouter>
-    )
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LoginLayout />}>
+          <Route path="login" element={<Login />} />
+          <Route path="/Singup" element={<Signup />} />
+        </Route>
+        <Route path="/" element={<BasicLayout />}>
+          <Route index element={<Main />} />
+          <Route path="/board" element={<Board />} />
+          <Route path="/chart" element={<Chart />} />
+          <Route path="/create" element={<Create />} />
+          
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default AppRouter;
