29fe7be29040ceeb153a334f16c40712bc66d448
yelim1215 <yerim1215jg@naver.com>
2023-04-18 02:27:55 +0900

    SignUp 중복확인 버튼, 유효성 검사, 입력창 비어있는지 확인 구현

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -5,7 +5,9 @@ import "../css/SignUp.css"
 export const SignUp = () => {
     const [id, setId] = useState('');
     const [password, setPassword] = useState('');
+    const [passwordError, setPasswordError] = useState('');
     const [nickname, setNickname] = useState('');
+    const [nicknameError, setNicknameError] = useState('');
 
     return (
         <div className="signup">
@@ -13,24 +15,66 @@ export const SignUp = () => {
                 <h2 className="title">SIGNUP</h2>
                 <hr></hr>
                 <div>
-                    <div class="input-container">
-                        <input placeholder='아이디' maxLength="10" class="check_input" type='text' name='id' value={id} onChange={(e) => { SignUpService.userIdChanged(e, setId) }} style={{ width: '200px' }} />
-                        <button class="btn">중복 확인</button>
+                    <div className="input-container">
+                        <input
+                            placeholder='아이디'
+                            maxLength="20"
+                            className="check_input"
+                            type='text'
+                            name='id'
+                            value={id}
+                            onChange={(e) => { SignUpService.userIdChanged(e, setId) }}
+                        />
+                        <button className="btn" onClick={(e) => { SignUpService.checkId(id, password, nickname) }}>중복 확인</button>
                     </div>
                 </div>
                 <div>
-                    <input placeholder='비밀번호' maxLength="20" type='password' name='password' value={password}
-                        onChange={(e) => { SignUpService.passwordChanged(e, setPassword) }} />
+                    <input
+                        placeholder='비밀번호'
+                        maxLength="20"
+                        type='password'
+                        name='password'
+                        value={password}
+                        onChange={(e) => {
+                            const value = e.target.value;
+                            setPassword(value);
+                            if (value.length < 8 || value.length > 20) {
+                                setPasswordError('비밀번호는 최소 8자리, 최대 20자리여야 합니다.');
+                            } else {
+                                setPasswordError('');
+                            }
+                        }}
+                    />
+                    {passwordError && <span className="error">{passwordError}</span>}
                 </div>
                 <div>
-                    <input placeholder='닉네임' type='text' name='nickname' value={nickname}
-                        onChange={(e) => { SignUpService.nickNameChanged(e, setNickname) }} />
+                    <input
+                        placeholder='닉네임'
+                        type='text'
+                        name='nickname'
+                        value={nickname}
+                        onChange={(e) => {
+                            const value = e.target.value;
+                            setNickname(value);
+                            if (!value) {
+                                setNicknameError('닉네임을 입력해주세요.');
+                            } else {
+                                setNicknameError('');
+                            }
+                        }}
+                    />
+                    {nicknameError && <span className="error">{nicknameError}</span>}
                 </div>
                 <div>
-                    <button className="signup_button" type='button'
-                        onClick={(e) => { SignUpService.signUpButtonClicked(id, password, nickname) }}>완료</button>
+                    <button
+                        className="signup_button"
+                        type='button'
+                        onClick={SignUpService.handleSignUp}
+                    >
+                        완료
+                    </button>
                 </div>
             </div>
         </div>
-    )
+    );
 }
--- src/service/SignUpService.js
+++ src/service/SignUpService.js
@@ -19,5 +19,41 @@ export const signUpButtonClicked = (id, password, nickname) => {
         nickname : nickname
     }
 
+    if (!id || !password || !nickname) {
+        alert("빈 칸을 모두 입력해주세요.");
+        return;
+    }
+
+    if (password.length < 8 || password.length > 20) {
+        alert('비밀번호는 최소 8자리, 최대 20자리여야 합니다.');
+        return;
+    }
+
     SignUpNetwork.signUp(newUser);
 }
+
+export const checkId = (id, password, nickname) => {
+    const newUser = {
+        userId : id,
+        pw : password,
+        nickname : nickname
+    }
+
+    SignUpNetwork.signUp(newUser);
+}
+
+export const handleSignUp = (id, password, nickname) => {
+    if (!id) {
+        alert('아이디를 입력해주세요.');
+        return;
+    }
+    if (!password) {
+        alert('비밀번호를 입력해주세요.');
+        return;
+    }
+    if (!nickname) {
+        alert('닉네임을 입력해주세요.');
+        return;
+    }
+    signUpButtonClicked(id, password, nickname);
+};
