fd7b7743aa63799463087424eec5a1ef778ed15c
yelim1215 <yerim1215jg@naver.com>
2023-04-17 17:42:50 +0900

    Login css 작성

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import * as LoginService from "../service/LoginService.js";
+import "../css/Login.css"
 
 export const Login = () => {
     const [userId, setUserId] = useState('')
@@ -7,24 +8,26 @@ export const Login = () => {
 
     return(
         <div className="login">
-        <div className='login-container'>
-            <h1 className="title">LOGIN</h1>
-            <div>
-                <input placeholder='아이디' type='text' name='id' value={userId}
-                       onChange={(e)=>{LoginService.userIdChanged(e, setUserId)}} />
-            </div>
-            <div>
-                <input placeholder='비밀번호' type='password' name='password' value={password}
-                       onChange={(e)=>{LoginService.userPasswordChanged(e, setPassword)}} />
-            </div>
-            <div>
-                <button type='button' onClick={(e)=>{LoginService.loginButtonClicked(e, userId, password)}}>로그인</button>
+            <div className='login-container'>
+                <h1 className="title">LOGIN</h1>
+                <hr></hr>
+                <div>
+                    <input placeholder='아이디' type='text' name='id' value={userId}
+                           onChange={(e)=>{LoginService.userIdChanged(e, setUserId)}} />
+                </div>
+                <div>
+                    <input placeholder='비밀번호' type='password' name='password' value={password}
+                           onChange={(e)=>{LoginService.userPasswordChanged(e, setPassword)}} />
+                </div>
+                <div>
+                    <button type='button' className="login-btn" onClick={(e)=>{LoginService.loginButtonClicked(e, userId, password)}}>로그인</button>
+                </div>
+                <div className="login-buttons">
+                    <button type='button' className="find-password-button">비밀번호 찾기</button>
+                    <button type='button' className="find-id-button">아이디 찾기</button>
+                    <a href="/SignUp"><button type='button' className="signup-button">회원가입</button></a>
+                </div>
             </div>
         </div>
-        
-        <div>
-            <p><a>비밀번호 찾기</a> | <a>아이디 찾기</a> | <a href="/SignUp">회원가입</a></p>
-        </div>
-    </div>
     )
 }
--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,34 +1,35 @@
-import React, { useState} from 'react';
+import React, { useState } from 'react';
 import * as SignUpService from "../service/SignUpService"
+import "../css/SignUp.css"
 
 export const SignUp = () => {
     const [id, setId] = useState('');
     const [password, setPassword] = useState('');
     const [nickname, setNickname] = useState('');
 
-    return(
-        <div class="signup">
-            <h2 class="title">SIGNUP</h2>
-            <hr></hr>
-            <div>
-                <label htmlFor='id'>아이디</label>
-                <input maxLength="10" class="check_input" type='text' name='id' value={id}
-                       onChange={(e)=>{SignUpService.userIdChanged(e,setId)}}/>
-                <button class="btn">중복 확인</button>
-            </div>
-            <div>
-                <label htmlFor='password'>비밀번호</label>
-                <input maxLength="20" type='password' name='password' value={password}
-                       onChange={(e)=>{SignUpService.passwordChanged(e,setPassword)}}/>
-            </div>
-            <div>
-                <label htmlFor='nickname'>닉네임</label>
-                <input type='text' name='nickname' value={nickname}
-                       onChange={(e)=>{SignUpService.nickNameChanged(e,setNickname)}} />
-            </div>
-            <div>
-                <button class="signup_button" type='button'
-                        onClick={(e)=>{SignUpService.signUpButtonClicked(id, password, nickname)}}>완료</button>
+    return (
+        <div className="signup">
+            <div className="signup-container">
+                <h2 className="title">SIGNUP</h2>
+                <hr></hr>
+                <div>
+                    <div class="input-container">
+                        <input placeholder='아이디' maxLength="10" class="check_input" type='text' name='id' value={id} onChange={(e) => { SignUpService.userIdChanged(e, setId) }} style={{ width: '200px' }} />
+                        <button class="btn">중복 확인</button>
+                    </div>
+                </div>
+                <div>
+                    <input placeholder='비밀번호' maxLength="20" type='password' name='password' value={password}
+                        onChange={(e) => { SignUpService.passwordChanged(e, setPassword) }} />
+                </div>
+                <div>
+                    <input placeholder='닉네임' type='text' name='nickname' value={nickname}
+                        onChange={(e) => { SignUpService.nickNameChanged(e, setNickname) }} />
+                </div>
+                <div>
+                    <button className="signup_button" type='button'
+                        onClick={(e) => { SignUpService.signUpButtonClicked(id, password, nickname) }}>완료</button>
+                </div>
             </div>
         </div>
     )
