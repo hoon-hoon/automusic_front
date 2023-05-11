0a07fc00169debf20a806dd3d7ab10fd238bd22c
yelim1215 <yerim1215jg@naver.com>
2023-05-11 23:20:46 +0900

    회원가입 페이지 이미지 추가

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -10,37 +10,42 @@ export const SignUp = () => {
     return (
         <div className="signup">
             <div className="signup-container">
-                <p className="title">회원가입</p>
-                <div class="input-container">
-                    <label htmlFor='id'>아이디</label>
-                    <hr className='hrBig'></hr>
-                    <input class="id" type='text' name='id' id="id" value={id} 
-                        onChange={(e) => { SignUpService.userIdChanged(e, setId) }} />
-                    <hr className='hrSmall'></hr>
+                <div className='form-container'>
+                    <p className="title">회원가입</p>
+                    <div class="input-container">
+                        <label htmlFor='id'>아이디</label>
+                        <hr className='hrBig'></hr>
+                        <input class="id" type='text' name='id' id="id" value={id} 
+                            onChange={(e) => { SignUpService.userIdChanged(e, setId) }} />
+                        <hr className='hrSmall'></hr>
+                    </div>
+                    <div class="input-container">
+                        <label htmlFor='password'>비밀번호</label>
+                        <hr className='hrBig'></hr>
+                        <input type='password' name='password' id="password" value={password}
+                            onChange={(e) => { SignUpService.passwordChanged(e, setPassword) }} />
+                        <hr className='hrSmall'></hr>
+                    </div>
+                    <div class="input-container">
+                        <label htmlFor='passwordCheck'>비밀번호 확인</label>
+                        <hr className='hrBig'></hr>
+                        <input type='password' name='passwordCheck' id="passwordCheck" />
+                        <hr className='hrSmall'></hr>
+                    </div>
+                    <div class="input-container">
+                        <label htmlFor='nickname'>닉네임</label>
+                        <hr className='hrBig'></hr>
+                        <input type='text' name='nickname' id="nickname" value={nickname}
+                            onChange={(e) => { SignUpService.nickNameChanged(e, setNickname) }} />
+                        <hr className='hrSmall'></hr>
+                    </div>
+                    <div>
+                        <button className="signup_button" type='button'
+                            onClick={(e) => { SignUpService.signUpButtonClicked(id, password, nickname) }}>회원가입</button>
+                    </div>
                 </div>
-                <div class="input-container">
-                    <label htmlFor='password'>비밀번호</label>
-                    <hr className='hrBig'></hr>
-                    <input type='password' name='password' id="password" value={password}
-                        onChange={(e) => { SignUpService.passwordChanged(e, setPassword) }} />
-                    <hr className='hrSmall'></hr>
-                </div>
-                <div class="input-container">
-                    <label htmlFor='passwordCheck'>비밀번호 확인</label>
-                    <hr className='hrBig'></hr>
-                    <input type='password' name='passwordCheck' id="passwordCheck" />
-                    <hr className='hrSmall'></hr>
-                </div>
-                <div class="input-container">
-                    <label htmlFor='nickname'>닉네임</label>
-                    <hr className='hrBig'></hr>
-                    <input type='text' name='nickname' id="nickname" value={nickname}
-                        onChange={(e) => { SignUpService.nickNameChanged(e, setNickname) }} />
-                    <hr className='hrSmall'></hr>
-                </div>
-                <div>
-                    <button className="signup_button" type='button'
-                        onClick={(e) => { SignUpService.signUpButtonClicked(id, password, nickname) }}>회원가입</button>
+                <div className='image-container'>
+                    <img className="signup-img" src="/images/signupImg.jpeg" alt="signupImg" loading="lazy" />
                 </div>
             </div>
         </div>
