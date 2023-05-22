aa5e3eefa42f8e3d962282310120ca3346e45e3d
yelim1215 <yerim1215jg@naver.com>
2023-05-23 03:11:15 +0900

    main 페이지 디자인 완성

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,4 +1,5 @@
 import "../css/main.css"
+import { IoIosArrowForward } from "react-icons/io";
 
 function Main() {
     return (<>
@@ -16,10 +17,73 @@ function Main() {
                 <p>저작권 없는 음악을</p>
                 <p>생성하고 추천해 보세요</p>
             </div>
+
             <div className="MYOM">MAKE YOUR OWN MUSIC.</div>
             <div className="copyright-text">
                 <p>자유롭게 저작권 걱정없는</p>
                 <p>음악을 생성해 보세요.</p>
+            </div>
+
+            <div><IoIosArrowForward className="arrow" /></div>
+
+            <div className="cmsfuc">Create music suitable for your content</div>
+
+            <div className="middle-content">
+                <p>AutoMusic</p>
+                <p>자유롭게 저작권 걱정없는 음악을 생성해보세요.</p>
+            </div>
+
+            <div className="whiteBox-all">
+                <div className="whiteBox-middle">
+                     <div className="mini">
+                        <p className="mini-title">프로젝트 목적</p>
+                        <p>
+                            AutoMusic
+                            이웃 이름과 흙으로 옥 다하지 풀이 언덕 거외다. 이런 써 어머님, 한 아침이
+                            별에도 자랑처럼 소녀들의 까닭입니다. 릴케 속의 마리아 까닭입니다. 흙으로
+                            아침이 별 없이 강아지, 위에 같이 있습니다. 보고, 때 계절이 봅니다. 지나고
+                            풀이 하나에 아스라히 계십니다. 소녀들의 그리워 별빛이 하나에 아무 헤일
+                            별들을 무덤 많은 봅니다. 한 나의 별에도 동경과 경, 있습니다. 
+                        </p>
+                     </div>
+                     <div className="mini">
+                        <p className="mini-title">데이터 셋</p>
+                        <p>
+                            AutoMusic
+                            이웃 이름과 흙으로 옥 다하지 풀이 언덕 거외다. 이런 써 어머님, 한 아침이
+                            별에도 자랑처럼 소녀들의 까닭입니다. 릴케 속의 마리아 까닭입니다. 흙으로
+                            아침이 별 없이 강아지, 위에 같이 있습니다. 보고, 때 계절이 봅니다. 지나고
+                            풀이 하나에 아스라히 계십니다. 소녀들의 그리워 별빛이 하나에 아무 헤일
+                            별들을 무덤 많은 봅니다. 한 나의 별에도 동경과 경, 있습니다. 
+                        </p>
+                     </div>
+                </div>
+                <div className="whiteBox-middle">
+                     <div className="mini">
+                        <p className="mini-title">콘텐츠에 맞는 연주법 생성</p>
+                        <p>
+                            AutoMusic
+                            이웃 이름과 흙으로 옥 다하지 풀이 언덕 거외다. 이런 써 어머님, 한 아침이
+                            별에도 자랑처럼 소녀들의 까닭입니다. 릴케 속의 마리아 까닭입니다. 흙으로
+                            아침이 별 없이 강아지, 위에 같이 있습니다. 보고, 때 계절이 봅니다. 지나고
+                            풀이 하나에 아스라히 계십니다. 소녀들의 그리워 별빛이 하나에 아무 헤일
+                            별들을 무덤 많은 봅니다. 한 나의 별에도 동경과 경, 있습니다. 
+                        </p>
+                     </div>
+                     <div className="mini">
+                        <p className="mini-title">연주법을 토대로 음악 생성</p>
+                        <p>
+                            AutoMusic
+                            이웃 이름과 흙으로 옥 다하지 풀이 언덕 거외다. 이런 써 어머님, 한 아침이
+                            별에도 자랑처럼 소녀들의 까닭입니다. 릴케 속의 마리아 까닭입니다. 흙으로
+                            아침이 별 없이 강아지, 위에 같이 있습니다. 보고, 때 계절이 봅니다. 지나고
+                            풀이 하나에 아스라히 계십니다. 소녀들의 그리워 별빛이 하나에 아무 헤일
+                            별들을 무덤 많은 봅니다. 한 나의 별에도 동경과 경, 있습니다. 
+                        </p>
+                     </div>
+                </div>
+
+                <div className="footer"></div>
             </div>
         </div>
     </>)
