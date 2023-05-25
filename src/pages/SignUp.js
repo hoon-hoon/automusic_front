b5ca2563bfa0e77376740a55433faf33e5cb1113
yelim1215 <yerim1215jg@naver.com>
2023-05-25 23:42:12 +0900

    회원가입 유효성 검사 통신 성공

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -30,26 +30,14 @@ export const SignUp = () => {
                         <label htmlFor='id'>아이디</label>
                         <hr className='hrBig'></hr>
                         <input class="id" type='text' name='id' id="id" value={id} 
-                            onChange={(e) => { SignUpService.userIdChanged(e, setId) }}
-                            minLength={8}
-                            required />
-                        {id.length < 8 
-                            ? <p>8자이상 입력해주세요</p>
-                            : <></>
-                        }
+                            onChange={(e) => { SignUpService.userIdChanged(e, setId) }} />
                         <hr className='hrSmall'></hr>
                     </div>
                     <div class="input-container">
                         <label htmlFor='password'>비밀번호</label>
                         <hr className='hrBig'></hr>
                         <input type='password' name='password' id="password" value={password}
-                            onChange={(e) => { SignUpService.passwordChanged(e, setPassword) }}
-                            minLength={8}
-                            required />
-                        {password.length < 8 
-                            ? <p>8자이상 입력해주세요</p>
-                            : <></>
-                        }
+                            onChange={(e) => { SignUpService.passwordChanged(e, setPassword) }} />
                         <hr className='hrSmall'></hr>
                     </div>
                     <div class="input-container">
@@ -57,10 +45,10 @@ export const SignUp = () => {
                         <hr className='hrBig'></hr>
                         <input type='password' name='passwordCheck' id="passwordCheck"
                             onChange={(e) => checkedPassword(e.target.value, password)} required/>
-                        {passwordCheck 
-                            ? (<p>일치</p>) 
-                            : (<p>불일치</p>)
-                        }
+                            {passwordCheck 
+                                ? (<p>일치</p>) 
+                                : (<p>불일치</p>)
+                            }
                         <hr className='hrSmall'></hr>
                     </div>
                     <div class="input-container">
@@ -68,16 +56,11 @@ export const SignUp = () => {
                         <hr className='hrBig'></hr>
                         <input type='text' name='nickname' id="nickname" value={nickname}
                             onChange={(e) => { SignUpService.nickNameChanged(e, setNickname) }} />
-                        {nickname.length < 2 
-                            ? <p>2자이상 입력해주세요</p>
-                            : <></>
-                        }
                         <hr className='hrSmall'></hr>
                     </div>
                     <div>
                         <button className="signup-button" type='button'
-                            onClick={(e) => { {passwordCheck&&(id.length>=8)&&(password.length>=8)&&(nickname>=2) ? SignUpService.signUpButtonClicked(id, password, nickname) : alert("다시 작성해주세요") }}}>회원가입</button>
-                            {/* onClick={(e) => { SignUpService.signUpButtonClicked(id, password, nickname)}}>회원가입</button> */}
+                            onClick={(e) => { SignUpService.signUpButtonClicked(id, password, nickname) }}>회원가입</button>
                     </div>
                     <div className='bottom-login-button'>
                             <div className="button-text">등록된 회원 정보가 있으신가요?</div>
